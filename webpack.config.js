8a423b2389fb7106d51c6022a6bab904022805ce
Kunuk Nykjær <kunukn@gmail.com>
2018-05-28 11:02:12 +0200

    add prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,68 +5,74 @@ const WebpackMd5Hash = require("webpack-md5-hash");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-module.exports = {
-  entry: { main: "./src/index.js" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[chunkhash].js"
-  },
-  devServer: {
-    port: 9000,
-    contentBase: path.join(__dirname, ""),
-    open: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: false }
+module.exports = (env, argv) => {
+  console.log("***", argv.mode, "***");
+
+  const isProd = argv.mode === "production";
+
+  return {
+    entry: { main: "./src/index.js" },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[chunkhash].js"
+    },
+    devServer: {
+      port: 9000,
+      contentBase: path.join(__dirname, ""),
+      open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: false }
+            }
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            isProd && MiniCssExtractPlugin.loader, // write to disc
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
+          ]
         }
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          "style-loader",
-          //MiniCssExtractPlugin.loader, // use when writing to file
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
-        ]
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin("dist", {}),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new HtmlWebpackPlugin({
+        inject: true,
+        hash: true,
+        template: "./src/index.html",
+        filename: "index.html"
+      }),
+      new WebpackMd5Hash()
+    ],
+    resolve: {
+      extensions: [".js", ".jsx", ".scss"],
+      alias: {
+        root: path.resolve(__dirname, "src"),
+        components: path.resolve(__dirname, "src/components")
       }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin("dist", {}),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      hash: true,
-      template: "./src/index.html",
-      filename: "index.html"
-    }),
-    new WebpackMd5Hash()
-  ],
-  resolve: {
-    extensions: [".js", ".jsx", ".scss"],
-    alias: {
-      root: path.resolve(__dirname, "src"),
-      components: path.resolve(__dirname, "src/components")
     }
-  }
+  };
 };
